f7978020348eaa37cce45f035c2f5d5ea4ff66cd
Don Juan Javier <townofdon@gmail.com>
2022-10-01 09:40:20 -0400

    add InputController and working prototype example

--- Assets/ScriptsDon/InputController.cs
+++ Assets/ScriptsDon/InputController.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // state
+    Vector2 move;
+    Vector2 look; // note - look only applies to gamepad R stick
+    Vector2 mousePositionScreen;
+    Vector2 mousePositionWorld; // mouse position in world relative to this GameObject's transform position
+    Vector2 mousePositionNormalized; // (0,0) lower LH corner -> (1,1) upper RH corner
 
-    // Update is called once per frame
+    // cached
+    new Camera camera;
+
     void Update()
     {
-        
+        if (camera == null) camera = Camera.main;
+        mousePositionScreen = Mouse.current.position.ReadValue();
+        mousePositionWorld = camera.ScreenToWorldPoint(mousePositionScreen) - transform.position;
+        mousePositionNormalized = camera.ScreenToViewportPoint(mousePositionScreen);
+
+        Debug.Log($"move={move}, look={look}, mouseNorm={mousePositionNormalized}, mouseWorld={mousePositionWorld}");
+    }
+
+    void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
+    }
+
+    void OnLook(InputValue value)
+    {
+        look = value.Get<Vector2>();
     }
 }
