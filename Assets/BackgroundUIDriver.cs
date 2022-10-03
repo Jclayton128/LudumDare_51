463201973579f1a5b444cc7059d41b5aaf762e67
jclayton128 <jclayton128@gmail.com>
2022-10-02 23:38:07 -0400

    Pulsing particle background in time to the countdown.

---
+++ Assets/BackgroundUIDriver.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundUIDriver : MonoBehaviour
+{
+    TimeController timeController;
+    [SerializeField] Color[] _phaseColors = new Color[3];
+
+    [SerializeField] ParticleSystem _upSystem = null;
+    [SerializeField] ParticleSystem _downSystem = null;
+    ParticleSystem.MainModule _upMain;
+    ParticleSystem.MainModule _downMain;
+    [SerializeField] int _count = 1000;
+
+    private void Awake()
+    {
+        timeController = FindObjectOfType<TimeController>();
+        timeController.OnTimerAdvancement += HandleOnTimerAdvanced;
+        timeController.OnNewPhase += HandlePhaseChange;
+
+        _upMain = _upSystem.main;
+        _downMain = _downSystem.main;
+    }
+
+    private void HandleOnTimerAdvanced(int count)
+    {
+        if (count % 2 == 0)
+        {
+            Debug.Log("up");
+            _upSystem.Emit(_count);
+            
+        }
+        else
+        {
+            Debug.Log("down");
+            _downSystem.Emit(_count);
+        }
+    }
+
+    private void HandlePhaseChange(TimeController.Phase newPhase)
+    {
+        Debug.Log($"new phase color: {_phaseColors[(int)newPhase]}");
+        _upMain.startColor = _phaseColors[ (int)newPhase];
+        _downMain.startColor = _phaseColors[(int)newPhase];
+    }
+}
