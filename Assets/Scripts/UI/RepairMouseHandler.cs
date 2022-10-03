e6fe1ac9af7b3206b52b8eed7c91d0c66514bcfa
jclayton128 <jclayton128@gmail.com>
2022-10-03 12:36:57 -0400

    Fixed issue with Health UI buttons being finicky

--- Assets/Scripts/UI/RepairMouseHandler.cs
+++ Assets/Scripts/UI/RepairMouseHandler.cs
@@ -12,6 +12,8 @@ public class RepairMouseHandler : MonoBehaviour
     [Space]
     [Header("UI / Event")]
 
+    TimeController _timeController;
+
     GraphicRaycaster m_Raycaster;
     EventSystem m_EventSystem;
 
@@ -20,34 +22,80 @@ public class RepairMouseHandler : MonoBehaviour
     GameObject player;
     StatsHandler stats;
     InputController input;
+    Button button;
 
     float timeDebounceFindPlayer = 0f;
+
+    bool _shouldBeRepairing = false;
 
     private void Awake()
     {
         FindPlayer();
         m_Raycaster = FindObjectOfType<GraphicRaycaster>();
         m_EventSystem = FindObjectOfType<EventSystem>();
+
+        _timeController = FindObjectOfType<TimeController>();
+        _timeController.OnNewPhase += HandlePhaseChange;
+        button = GetComponent<Button>();
+        button.interactable = false;
+
     }
+
+    private void HandlePhaseChange(TimeController.Phase newPhase)
+    {
+        if (newPhase == TimeController.Phase.C_healing)
+        {
+            button.interactable = true;
+        }
+        else
+        {
+            button.interactable = false;
+        }
+    }
+
+    public void OnPointerDown()
+    {
+        if (!input.IsFirePressed) return;
+        Debug.Log($"pointer is down on system {subsystemIndex}");
+        _shouldBeRepairing = true;
+    }
+
+    public void OnPointerExit()
+    {
+        Debug.Log($"pointer is exiting system {subsystemIndex}");
+        _shouldBeRepairing = false;
+    }
+
+    public void OnPointerUp()
+    { 
+        Debug.Log($"pointer is released from system {subsystemIndex}");
+        _shouldBeRepairing = false;
+    }
+
 
     void Update()
     {
-        if (player == null)
+        if (_shouldBeRepairing)
         {
-            FindPlayer();
-            return;
+            stats.RepairDamage(subsystemIndex);
         }
-        // SOLUTION BELOW FROM THIS FORUM THREAD: https://forum.unity.com/threads/how-to-raycast-onto-a-unity-canvas-ui-image.855259/
-        if (!input.IsFirePressed) return;
-        //Set up the new Pointer Event
-        m_PointerEventData = new PointerEventData(m_EventSystem);
-        //Set the Pointer Event Position to that of the game object
-        m_PointerEventData.position = input.MousePositionScreen;
-        //Create a list of Raycast Results
-        List<RaycastResult> results = new List<RaycastResult>();
-        //Raycast using the Graphics Raycaster and mouse click position
-        m_Raycaster.Raycast(m_PointerEventData, results);
-        if (results.Count > 0) OnHit(results[0].gameObject);
+
+        //if (player == null)
+        //{
+        //    FindPlayer();
+        //    return;
+        //}
+        //// SOLUTION BELOW FROM THIS FORUM THREAD: https://forum.unity.com/threads/how-to-raycast-onto-a-unity-canvas-ui-image.855259/
+        //if (!input.IsFirePressed) return;
+        ////Set up the new Pointer Event
+        //m_PointerEventData = new PointerEventData(m_EventSystem);
+        ////Set the Pointer Event Position to that of the game object
+        //m_PointerEventData.position = input.MousePositionScreen;
+        ////Create a list of Raycast Results
+        //List<RaycastResult> results = new List<RaycastResult>();
+        ////Raycast using the Graphics Raycaster and mouse click position
+        //m_Raycaster.Raycast(m_PointerEventData, results);
+        //if (results.Count > 0) OnHit(results[0].gameObject);
     }
 
     void OnHit(GameObject raycastedGameObject)
